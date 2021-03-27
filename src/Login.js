041627c6caefbdc9905d879c6662c21f6da14394
Mahesh Ranpise <mahesh8276@gmail.com>
2021-03-28 01:56:46 +0530

    V1

---
+++ src/Login.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./Login.css";
+import { Container } from "react-bootstrap"
+
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=8b945ef10ea24755b83ac50cede405a0&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+export default function Login() {
+  return (
+      <div className="login">
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <a href={AUTH_URL}>LOGIN TO SPOTIFY</a>
+    </div>
+  )
+}
